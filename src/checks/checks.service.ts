cf3817107b68112dd457f59f93d53ad6e09f4882
Aiden Yang <aidenhsy@gmail.com>
2025-07-28 09:47:35 +0900

    updated scheduled checks

--- src/checks/checks.service.ts
+++ src/checks/checks.service.ts
@@ -47,6 +47,8 @@ export class ChecksService {
         missingProcurementOrders.map((order) => order.client_order_id),
       );
     }
+
+    this.logger.log('Checking different orders done');
   }
 
   @Cron('42 11 * * *', {
@@ -96,6 +98,8 @@ export class ChecksService {
         }
       }
     }
+
+    this.logger.log('Checking pricings done');
   }
 
   @Cron('50 11 * * *', {
@@ -126,5 +130,98 @@ export class ChecksService {
         );
       }
     }
+
+    this.logger.log('Checking im scm sync pricings done');
+  }
+
+  @Cron('00 13 * * *', {
+    timeZone: 'Asia/Shanghai',
+  })
+  async checkDeliveryQty() {
+    this.logger.log('Checking delivery qty');
+
+    const batchSize = 100;
+    let skip = 0;
+    let hasMoreOrders = true;
+
+    while (hasMoreOrders) {
+      const orders =
+        await this.databaseService.order.procurement_orders.findMany({
+          select: {
+            client_order_id: true,
+            procurement_order_details: {
+              select: {
+                reference_id: true,
+                deliver_qty: true,
+              },
+            },
+          },
+          take: batchSize,
+          skip: skip,
+        });
+
+      if (orders.length < batchSize) {
+        hasMoreOrders = false;
+      }
+
+      if (orders.length === 0) {
+        break;
+      }
+
+      const procurementOrders =
+        await this.databaseService.procurement.supplier_orders.findMany({
+          where: {
+            id: {
+              in: orders.map((order) => order.client_order_id),
+            },
+          },
+          select: {
+            id: true,
+            supplier_order_details: {
+              select: {
+                id: true,
+                supplier_reference_id: true,
+                actual_delivery_qty: true,
+              },
+            },
+          },
+        });
+
+      for (const order of orders) {
+        const procurementOrder = procurementOrders.find(
+          (o) => o.id === order.client_order_id,
+        );
+
+        if (!procurementOrder) {
+          console.log(`${order.client_order_id} not found`);
+          continue;
+        }
+
+        for (const orderDetail of order.procurement_order_details) {
+          const procurementDetail =
+            procurementOrder.supplier_order_details.find(
+              (d) => d.supplier_reference_id === orderDetail.reference_id,
+            );
+          if (!procurementDetail) {
+            console.log(`${orderDetail.reference_id} not found`);
+            continue;
+          }
+          if (
+            Number(procurementDetail.actual_delivery_qty) !==
+            Number(orderDetail.deliver_qty)
+          ) {
+            console.log(
+              `${orderDetail.reference_id} difference ${procurementDetail.actual_delivery_qty} ${orderDetail.deliver_qty} \n id: ${order.client_order_id} \n `,
+            );
+            console.log('-----------');
+          }
+        }
+      }
+
+      // Move to the next batch
+      skip += batchSize;
+    }
+
+    this.logger.log('Checking delivery qty done');
   }
 }
