a1a89ab5c16496bca9b5b7ff41e5a5fe744b4ec1
aiden <aidenhsy@gmail.com>
2025-08-06 07:02:28 +0800

    update

--- src/checks/checks.service.ts
+++ src/checks/checks.service.ts
@@ -138,7 +138,7 @@ export class ChecksService {
     this.logger.log('Checking im scm sync pricings done');
   }
 
-  @Cron('0 8 * * *', {
+  @Cron('0 * * * *', {
     timeZone: 'Asia/Shanghai',
   })
   async checkDeliveryQty() {
